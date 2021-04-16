e7be2785e846f54608170bf8119269a2d28dc4ae
idleberg <git@idleberg.com>
2021-04-17 00:13:58 +0200

    build

--- lib/package-developer-toolbar.d.ts
+++
@@ -1,4 +0,0 @@
-export { config } from './config';
-export declare function activate(): Promise<void>;
-export declare function deactivate(): void;
-export declare function consumeToolBar(getToolBar: any): any;
--- lib/toolbar.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function helloWorld(): import("atom").Notification;
