c78438631c6a1a4e4201447b6611d042a948cb4f
Jan T. Sott <git@idleberg.com>
2024-09-29 09:49:09 +0200

    update dependencies

--- src/log.ts
+++ src/log.ts
@@ -1,7 +1,6 @@
 import meta from '../package.json';
 import DeveloperConsole from '@atxm/developer-console';
 
-export default new DeveloperConsole({
-  name: meta.name,
+export default new DeveloperConsole(meta.name, {
   backgroundColor: 'gray'
 });
